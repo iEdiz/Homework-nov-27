75bd97a08519a108c79703778e10c2e039f23345
Edijs Mezits <edijs.mezits@gmail.com>
2023-11-26 16:08:56 +0200

    Fixed searched, added sort function

--- html_ts_boilerplate-master/src/scripts.ts
+++ html_ts_boilerplate-master/src/scripts.ts
@@ -5,57 +5,85 @@ const countryWrapper: HTMLDivElement = document.querySelector('.js-country-wrapp
 const countryHeader: HTMLDivElement = document.querySelector('.js-country-header');
 
 type Country = {
-    id?: string;
-    name: string;
+  id?: string;
+  name: string;
+  code: string;
+  capital: string;
+  currency: {
     code: string;
-    capital: string;
-    currency: {
-        name: string;
-    };
-    language: {
-        name: string;
-    };
+    name: string;
+    symbol: string;
+  };
+  language: {
+    code: string;
+    name: string;
+  };
+  flag: string;
+  dialling_code: string;
+  isoCode: string;
 };
 
-let countryNamesArray: string[] = [];
+let countriesArr: Country[] = [];
 
 const getData = async (): Promise<void> => {
   try {
     const response = await axios.get<Country[]>('http://localhost:3004/countries');
-    const countries: Country[] = response.data;
-
-    const countriesWithIds = countries.map((country: Country) => ({
-      id: uuidv4(),
-      ...country,
-    }));
-
-    countryNamesArray = Array.from(new Set(countries.map((country: Country) => country.name)));
+    countriesArr = response.data.map((country) => ({ id: uuidv4(), ...country }));
 
     if (countryWrapper) {
-      countryWrapper.innerHTML = buildTableHTML(countriesWithIds);
+      countryWrapper.innerHTML = buildTableHTML(countriesArr);
     }
   } catch (error) {
     console.error('Error fetching countries:', error);
   }
 
-  const searchButton = document.querySelector<HTMLButtonElement>('.js-search-button');
+  const searchByCountryName = (value: string) => {
+    const filteredCountries = countriesArr.filter(
+      (country) => country.name.toLowerCase().includes(value.toLowerCase()),
+    );
+    showCountries(filteredCountries);
+  };
+
+  const searchByCapitalCity = (value: string) => {
+    const filteredCountries = countriesArr.filter(
+      (country) => country.capital.toLowerCase().includes(value.toLowerCase()),
+    );
+    showCountries(filteredCountries);
+  };
+
+  const searchByCurrency = (value: string) => {
+    const filteredCountries = countriesArr.filter(
+      (country) => country.currency.name.toLowerCase().includes(value.toLowerCase()),
+    );
+    showCountries(filteredCountries);
+  };
+
+  const searchByLanguage = (value: string) => {
+    const filteredCountries = countriesArr.filter(
+      (country) => country.language.name.toLowerCase().includes(value.toLowerCase()),
+    );
+    showCountries(filteredCountries);
+  };
+
   const searchCountryName = document.querySelector<HTMLInputElement>('input[name="countryName"]');
+  const searchCapitalCity = document.querySelector<HTMLInputElement>('input[name="countryCapital"]');
+  const searchCurrency = document.querySelector<HTMLInputElement>('input[name="countryCurrency"]');
+  const searchLanguage = document.querySelector<HTMLInputElement>('input[name="countryLanguage"]');
 
-  searchButton.addEventListener('click', () => {
-    const searchCountryNameValue = searchCountryName.value.trim().toLowerCase();
+  searchCountryName.addEventListener('input', () => {
+    searchByCountryName(searchCountryName.value.trim());
+  });
 
-    console.log('Searching for:', searchCountryNameValue);
+  searchCapitalCity.addEventListener('input', () => {
+    searchByCapitalCity(searchCapitalCity.value.trim());
+  });
 
-    countryNamesArray.forEach((countryName) => {
-      console.log('Current country:', countryName);
+  searchCurrency.addEventListener('input', () => {
+    searchByCurrency(searchCurrency.value.trim());
+  });
 
-      const isVisible = countryName.trim().toLowerCase().includes(searchCountryNameValue);
-
-      const countryElement = document.getElementById(`country-${countryName}`);
-      if (countryElement) {
-        countryElement.closest('tr').classList.toggle('hide', !isVisible);
-      }
-    });
+  searchLanguage.addEventListener('input', () => {
+    searchByLanguage(searchLanguage.value.trim());
   });
 };
 
@@ -64,49 +92,87 @@ const mainHeader = () => {
     <h2 class="main-header">My Country List</h2>
     <form class="d-flex input-group w-auto js-input" style="gap: 20px; margin: 0 0 10px 0">
         <input class="form-control" type="text" name="countryName" placeholder="Search country name..." value="">
+        <span class="icon-arrow js-icon-arrow" data-sort="name">&UpArrow;</span>
         <input class="form-control" type="text" name="countryCapital" placeholder="Search capital city...">
+        <span class="icon-arrow js-icon-arrow" data-sort="capital">&UpArrow;</span>
         <input class="form-control" type="text" name="countryCurrency" placeholder="Search currency...">
         <input class="form-control" type="text" name="countryLanguage" placeholder="Search language...">
-        <button type="button" class="search-button js-search-button">Search</button>
-        <button type="reset" class="reset-button">Reset</button>
     </form>
     `;
 };
 
-getData();
-mainHeader();
+const showCountries = (filteredCountries: Country[]) => {
+  const countryTable = document.querySelector('.country-table');
 
-// Function to build the HTML table
-function buildTableHTML(countries: Country[]): string {
-  const uniqueCountryNames = Array.from(new Set(countries.map((country) => country.name)));
+  if (countryTable) {
+    countryTable.innerHTML = buildTableHTML(filteredCountries);
+  }
+};
 
-  let html = `<table class="table table-striped table-dark table-bordered table-hover">
-        <thead class="thead-dark">
-            <tr>
-                <th scope="col">Country name</th>
-                <th scope="col">Country code</th>
-                <th scope="col">Capital city</th>
-                <th scope="col">Currency</th>
-                <th scope="col">Language</th>
-            </tr>
-        </thead>
-        <tbody>`;
+const buildTableHTML = (countries: Country[]): string => {
+  const html = `
+    <table class="table table-striped table-dark table-bordered table-hover country-table">
+      <thead class="thead-dark">
+        <tr>
+          <th scope="col" class="js-table-heading">Country name</th>
+          <th scope="col" class="js-table-heading">Country code</th>
+          <th scope="col" class="js-table-heading">Capital city</th>
+          <th scope="col" class="js-table-heading">Currency</th>
+          <th scope="col" class="js-table-heading">Language</th>
+        </tr>
+      </thead>
+      <tbody>
+        ${countries
+    .map(
+      (country) => `
+              <tr class="js-country-row" id="country-${country.id}">
+                <td>${country.name}</td>
+                <td>${country.code}</td>
+                <td>${country.capital}</td>
+                <td>${country.currency.name}</td>
+                <td>${country.language.name}</td>
+              </tr>
+            `,
+    )
+    .join('')}
+      </tbody>
+    </table>`;
 
-  uniqueCountryNames.forEach((countryName) => {
-    const country = countries.find((c) => c.name === countryName);
+  return html;
+};
 
-    if (country) {
-      html += `
-                <tr id="country-${country.name}">
-                    <td>${country.name}</td>
-                    <td>${country.code}</td>
-                    <td>${country.capital}</td>
-                    <td>${country.currency.name}</td>
-                    <td>${country.language.name}</td>
-                </tr>`;
-    }
+const sortData = (key: keyof Country) => {
+  countriesArr.sort((a, b) => {
+    const valueA = a[key].toString().toLowerCase();
+    const valueB = b[key].toString().toLowerCase();
+    return valueA.localeCompare(valueB);
   });
 
-  html += '</tbody></table>';
-  return html;
-}
+  showCountries(countriesArr);
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  getData();
+  mainHeader();
+
+  const arrows = document.querySelectorAll('.js-icon-arrow');
+
+  arrows.forEach((arrow) => {
+    arrow.addEventListener('click', () => {
+      arrows.forEach((otherArrow) => {
+        if (otherArrow !== arrow) {
+          otherArrow.classList.remove('down', 'active');
+        }
+      });
+
+      arrow.classList.toggle('active');
+      arrow.classList.toggle('down', arrow.classList.contains('active'));
+
+      const sortBy = arrow.getAttribute('data-sort') as keyof Country | null;
+
+      if (sortBy) {
+        sortData(sortBy);
+      }
+    });
+  });
+});
